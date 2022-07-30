2ecc0ba01e97bc9fa1ba1c8eba37d46091fa6d77
Ionut Ciovica <ionut.ciovica@gmail.com>
2022-07-30 08:58:12 +0100

    Initial Commit

---
+++ Mood Tracker by CIC/Mood_Tracker_by_CICApp.swift
@@ -0,0 +1,17 @@
+//
+//  Mood_Tracker_by_CICApp.swift
+//  Mood Tracker by CIC
+//
+//  Created by Ionut Ciovica on 30/07/2022.
+//
+
+import SwiftUI
+
+@main
+struct Mood_Tracker_by_CICApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
